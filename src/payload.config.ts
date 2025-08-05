a7003f5ce51132e948bf90f5b8873d5f5c25a688
trnganhson <tnason0901@gmail.com>
2025-08-05 23:16:48 +0700

    fix link of cart button

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -49,6 +49,7 @@ export default buildConfig({
     multiTenantPlugin<Config>({
       collections: {
         products: {},
+        media: {},
       },
       tenantsArrayField: {
         includeDefaultField: false,
